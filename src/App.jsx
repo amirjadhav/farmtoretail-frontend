766fd7525a8a0c54fd9ba16438687954600c201c
Pravin Shedbale <pravin.shedbale@outlook.com>
2023-05-04 12:43:51 +0530

    public folder changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
+import SignIn from "./pages/SignIn/SignIn";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <SignIn />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/pages/SignIn/SignIn.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const colors = {};
+
+const SignIn = () => {
+  return <div className="w-full h-full flex items-start">SignIn</div>;
+};
+
+export default SignIn;
