c324a958bb0ad8b9c2f3c413e278041a5fec2eab
Pravin Shedbale <pravin.shedbale@outlook.com>
2023-05-13 22:43:22 +0530

    login and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Header from "./components/Header/Header";
 import SignIn from "./pages/SignIn/SignIn";
 const App = () => {
   return (
     <div>
+      <Header />
       <SignIn />
     </div>
   );
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+import TopHeader from "./TopHeader";
+
+const Header = () => {
+  return (
+    <div>
+      <TopHeader />
+      <Navbar />
+    </div>
+  );
+};
+
+export default Header;
